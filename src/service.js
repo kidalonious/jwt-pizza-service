5c606938bf5b54cd4371cefa5519e4e652d0f17b
kidalonious <120538119+kidalonious@users.noreply.github.com>
2025-03-20 19:58:25 -0600

    reverted previous change

--- src/service.js
+++ src/service.js
@@ -45,7 +45,7 @@ app.use('*', (req, res) => {
 });
 
 // Default error handler for all exceptions and errors.
-app.use((err, _req, res, next) => {
+app.use((err, req, res, next) => {
   res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
   next();
 });
